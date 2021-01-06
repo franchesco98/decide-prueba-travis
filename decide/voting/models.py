a69e7332c689d382ab644241f79faaef49aa5a0d
jaime <jailucloz@alum.us.es>
2021-01-07 00:42:07 +0100

    Feature: implementada CRUD Political Party
    Implementada CRUD Political Party y puesta como campo en Voting

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -63,6 +63,8 @@ class Voting(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True, null=True)
     question = models.ForeignKey(Question, related_name='voting', on_delete=models.CASCADE)
+    political_party = models.ForeignKey(PoliticalParty, related_name='voting', on_delete=models.CASCADE, null=True)
+    # ,blank=True
 
     start_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField(blank=True, null=True)
