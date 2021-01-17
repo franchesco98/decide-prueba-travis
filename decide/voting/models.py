27d02e5b3bba049f856959d31c88b7bcdf3c53bd
Francisco Javier <frasanmat1@alum.us.es>
2021-01-17 23:27:25 +0100

    Feature: Añadidas las preguntas 'Yes or No' al crear una votación

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -80,12 +80,13 @@ class QuestionOption(models.Model):
 class Voting(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True, null=True)
-    question = models.ForeignKey(Question, related_name='voting', on_delete=models.CASCADE)
-    political_party = models.ForeignKey(PoliticalParty, related_name='voting', on_delete=models.CASCADE, null=True)
+    question = models.ForeignKey(Question, related_name='voting', on_delete=models.CASCADE, null=True, blank=True)
+    political_party = models.ForeignKey(PoliticalParty, related_name='voting', on_delete=models.CASCADE, null=True, blank=True)
     # ,blank=True
     
-    order_question = models.ForeignKey(OrderQuestion, related_name='voting', on_delete=models.CASCADE, null= True)
+    order_question = models.ForeignKey(OrderQuestion, related_name='voting', on_delete=models.CASCADE, null= True, blank=True)
 
+    yes_or_no_question = models.ForeignKey(YesOrNoQuestion, related_name='voting', on_delete=models.CASCADE, null=True, blank=True)
 
     start_date = models.DateTimeField(blank=True, null=True)
     end_date = models.DateTimeField(blank=True, null=True)
